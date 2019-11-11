f037b082acc7c38801eb8cf00a7a5e43bae56baf
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-11 11:47:04 -0700

    adding ship and testing

---
+++ src/js/ship.js
@@ -0,0 +1,43 @@
+const ship = {
+  generatorOne: (positions) => {
+    return {
+      float: true,
+      length: 1,
+      pos: [{ x: positions[0].x, y: positions[0].y, ok: true }]
+    };
+  },
+  generatorTwo: (positions) => {
+    return {
+      float: true,
+      length: 2,
+      pos: [
+        { x: positions[0].x, y: positions[0].y, ok: true },
+        { x: positions[1].x, y: positions[1].y, ok: true }
+      ]
+    };
+  },
+  generatorThree: (positions) => {
+    return {
+      float: true,
+      length: 3,
+      pos: [
+        { x: positions[0].x, y: positions[0].y, ok: true },
+        { x: positions[1].x, y: positions[1].y, ok: true },
+        { x: positions[2].x, y: positions[2].y, ok: true }
+      ]
+    };
+  },
+  isSunk: (boat) => {
+    const sunk = boat.pos.filter((element) => !element.ok);
+    if (sunk.length === boat.length) boat.float = false;
+  },
+
+  hit: (boat, xx, yy) => {
+    boat.pos.map((item) => {
+      if (xx == item.x && yy == item.y) item.ok = false;
+    });
+    ship.isSunk(boat);
+  }
+};
+
+module.exports = ship;
---
+++ src/test/ship.test.js
@@ -0,0 +1,58 @@
+const ship = require('../js/ship');
+
+const obj2 = ship.generatorThree([
+  { x: 0, y: 0 },
+  { x: 0, y: 1 },
+  { x: 0, y: 2 }
+]);
+
+test('ship object exists', () => {
+  expect(obj2).toBeDefined();
+});
+
+describe('testing a ship integrity', () => {
+  test('ship status is true for init', () => {
+    expect(obj2.float).toBe(true);
+  });
+  test('ship length is correct for init', () => {
+    expect(obj2.length).toBe(3);
+  });
+  test('ship length can not be other than specified', () => {
+    expect(obj2.length).not.toBe(2);
+  });
+  test('ship length can not be other than specified', () => {
+    expect(obj2.length).not.toBe(1);
+  });
+  test('ship position are correct', () => {
+    expect(obj2.pos[0].x).toBe(0);
+    expect(obj2.pos[0].y).toBe(0);
+    expect(obj2.pos[1].x).toBe(0);
+    expect(obj2.pos[1].y).toBe(1);
+    expect(obj2.pos[2].x).toBe(0);
+    expect(obj2.pos[2].y).toBe(2);
+  });
+  test('ship position can not be any other value than specified', () => {
+    expect(obj2.pos[0].x).not.toBe(1);
+    expect(obj2.pos[1].y).not.toBe(2);
+    expect(obj2.pos[2].x).not.toBe(3);
+  });
+});
+
+ship.hit(obj2, 0, 0);
+ship.hit(obj2, 0, 3);
+
+describe('testing a ship functions', () => {
+  test('when a ship is impacted if position exists', () => {
+    expect(obj2.pos[0].ok).toBe(false);
+  });
+  test('a ship is not impacted if position doesnt exists', () => {
+    expect(obj2.pos[0].ok).toBe(false);
+    expect(obj2.pos[1].ok).toBe(true);
+    expect(obj2.pos[2].ok).toBe(true);
+  });
+  test('a ship is sunk if all positions are hit', () => {
+    ship.hit(obj2, 0, 1);
+    ship.hit(obj2, 0, 2);
+    expect(obj2.float).toBe(false);
+  });
+});
