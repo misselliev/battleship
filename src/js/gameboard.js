6fddbcec3240e49a8e95b4f13e30a41a4e11807d
Santiago Torres G <thdealer@gmail.com>
2019-11-13 17:25:24 -0500

    removing redundant property: hit

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -4,57 +4,53 @@ const _ship = ship;
 const computeIdx = (posX, posY) => posX * boardSize + posY;
 const boardSize = 5;
 
-export const game = {
+export const board = {
   layout: [],
-  shipPositions: [],
-  missedHits: [],
-  board: () => {
+  grid: () => {
     for (let i = 0; i < boardSize; i += 1) {
       for (let j = 0; j < boardSize; j += 1) {
-        game.layout.push({
+        board.layout.push({
           x: i,
           y: j,
           water: true,
           shipId: "",
-          hit: false,
           used: false,
           length: 0
         });
       }
     }
-    return game.layout;
+    return board.layout;
   },
-  getShips: () => game.layout.filter(cell => !cell.water),
-  getUsedCells: () => game.layout.filter(cell => cell.used),
+  getShips: () => board.layout.filter(cell => !cell.water),
+  getUsedCells: () => board.layout.filter(cell => cell.used),
   setUsedCell: (posX, posY) => {
     const idx = computeIdx(posX, posY);
-    const cell = game.layout[idx];
+    const cell = board.layout[idx];
     cell.used = true;
-    if (!cell.hit) cell.hit = true;
   },
   getMissedHits: () => {
-    return game.layout.filter(cell => (cell.used && cell.water));
+    return board.layout.filter(cell => (cell.used && cell.water));
   },
-  waterPosition: () => game.layout.filter(cell => cell.water),
+  waterPosition: () => board.layout.filter(cell => cell.water),
   setShipPosition: ship => {
     Object.keys(ship.pos).forEach(key => {
       const idx = computeIdx(ship.pos[key].x, ship.pos[key].y);
-      game.layout[idx].water = false;
-      game.layout[idx].shipId = ship.id;
-      game.layout[idx].length = ship.length;
+      board.layout[idx].water = false;
+      board.layout[idx].shipId = ship.id;
+      board.layout[idx].length = ship.length;
     });
   },
   receiveAttack: (posX, posY) => {
-    const myBoats = game.getShips();
+    const myBoats = board.getShips();
     const temp = myBoats.filter(ship => ship.x == posX && ship.y == posY);
+    board.setUsedCell(posX, posY);
     if (temp) {
-      game.setUsedCell(posX, posY);
       _ship.hit(posX, posY);
     } else {
-      game.setMissedHits(posX, posY);
+      board.setMissedHits(posX, posY);
     }
   },
   gameOver: () => {
-    return game.getShips().every(cell => cell.hit);
+    return board.getShips().every(cell => cell.used);
   }
 };
