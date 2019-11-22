687ad1ac79d618ca765342be11ba7439664f6805
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-21 18:59:03 -0700

    cleaning logs

--- src/test/player.test.js
+++ src/test/player.test.js
@@ -18,27 +18,3 @@ beforeAll(() => {
 test("player object exists", () => {
   expect(init.humanPlayer).toBeDefined();
 });
-// describe("check player integrity", () => {
-//   test("player can shoot", () => {
-//     init.humanPlayer.shoot(0, 0, "human", init.humanObj);
-//     expect(init.humanObj.getGrid()[0].used).toBe(true);
-//   });
-//   test("player can shoot and miss", () => {
-//     init.humanPlayer.shoot(1, 4, "human", init.humanObj);
-//     expect(init.humanObj.getGrid()[21].used).toBe(true);
-//   });
-//   test("random() ", () => {
-//     expect(init.humanObj.getUsedCells().length).toBe(2);
-//     init.humanPlayer.randomShoot("human", init.humanObj);
-//     expect(init.humanObj.getUsedCells().length).toBe(3);
-//   });
-// test("bonus() increment the used cells while is hitting ships", () => {
-//   const numberOfCells = init.humanPlayer.bonus(
-//     true,
-//     "computer",
-//     init.humanObj
-//   );
-//   const usedSize = init.humanObj.getUsedCells().length;
-//   expect(numberOfCells).toBe(usedSize);
-// });
-// });
