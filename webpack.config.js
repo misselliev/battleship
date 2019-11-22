d55ef5e8a151357901dd89aa474d9015413e62b8
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-22 16:25:31 -0700

    updating webpack for resolving modules

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
     //   template: 'src/createProjectModal.html'
     // }),
   ],
+  resolve: {
+    modules: [__dirname, "node_modules"],
+    extensions: ["*", ".js", ".jsx"]
+  },
   module: {
     rules: [
       {
